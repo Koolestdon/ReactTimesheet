464d4a0b8e4ef1e77bd50814a3e9dea99efbe957
unknown <kevints.m@gmail.com>
2020-10-17 19:18:01 +0200

    commit

---
+++ src/webparts/timesheetWebPart/Model/IReactTimesheet.ts
@@ -0,0 +1,23 @@
+​import { Dropdown, IDropdownOption } from 'office-ui-fabric-react/lib/components/Dropdown';
+  
+export interface IReactTimesheet {
+    selectedItems: any[];
+    name: string;
+    description: string;
+    dpselectedItem?: { key: string | number | undefined };
+    termKey?: string | number;
+    dpselectedItems: IDropdownOption[];
+    disableToggle: boolean;
+    defaultChecked: boolean;
+    pplPickerType:string;
+    userIDs: number[];
+    userManagerIDs: number[];
+    hideDialog: boolean;
+    status: string;
+    isChecked: boolean;
+    showPanel: boolean;
+    required:string;
+    onSubmission:boolean;
+    termnCond:boolean;
+    
+}
