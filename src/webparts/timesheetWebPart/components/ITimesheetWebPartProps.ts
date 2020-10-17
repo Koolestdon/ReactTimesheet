113565256401e9084ced0b5968d8f4ba6f8141da
unknown <kevints.m@gmail.com>
2020-10-17 18:42:22 +0200

    first commit

---
+++ src/webparts/timesheetWebPart/components/ITimesheetWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface ITimesheetWebPartProps {
+  description: string;
+}
---
+++ src/webparts/timesheetWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITimesheetWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TimesheetWebPartWebPartStrings' {
+  const strings: ITimesheetWebPartWebPartStrings;
+  export = strings;
+}
